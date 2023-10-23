495aadbdcf9dd2ce7341a5cde47a58b37e4ddbf8
Andre <andrensaraiva@hotmail.com>
2023-10-23 11:17:03 -0300

    Cadastrar, Listar e Atualizar

---
+++ CRUDBasico/src/crudbasico/CRUDBasico.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package crudbasico;
+
+import Telas.TelaListagem;
+
+/**
+ *
+ * @author asaraiva
+ */
+public class CRUDBasico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaListagem tela = new TelaListagem();
+        tela.setVisible(true);
+        
+    }
+    
+}
